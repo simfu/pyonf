c91548e46139c4419672b390a74f9f111d5b8b91
simfu <simfu@free.fr>
2019-11-22 18:03:06 +0100

    Formatting

--- pyonf/__init__.py
+++ pyonf/__init__.py
@@ -1,27 +1,32 @@
 from __future__ import print_function
-import yaml
 import argparse
 import sys
 import os
 import logging
+import yaml
 
 
 log = logging.getLogger(__name__)
 
 
-def _deep_update(d, u):
+def _deep_update(in_dict, _update):
     """
         Recursively update a dict with another, returns the result
     """
-    for k, v in u.items():
-        if isinstance(v, dict):
-            d[k] = _deep_update(d.get(k, {}), v)
+    for key, val in _update.items():
+        if isinstance(val, dict):
+            in_dict[key] = _deep_update(in_dict.get(key, {}), val)
+        else:
+            in_dict[key] = val
+    return in_dict
+
+
         else:
             d[k] = v
     return d
 
 
-def _dict_to_args(d):
+def _dict_to_args(in_dict):
     """
         Convert nested dict keys to argparsable argument strings
 
@@ -30,10 +35,11 @@ def _dict_to_args(d):
         {"k1-k2-k3": v}
     """
     args = []
-    for arg, val in d.items():
+    for arg, val in in_dict.items():
         if isinstance(val, dict):
-            args += [(str(arg)+"-"+str(argd), vald)
-                     for argd, vald in _dict_to_args(val)]
+            args += [
+                (str(arg) + "-" + str(argd), vald) for argd, vald in _dict_to_args(val)
+            ]
         else:
             args.append((str(arg), val))
     return args
@@ -46,7 +52,7 @@ def _args_to_dict(d_arg, d_orig={}):
         :param d_orig: used to infer original keys' name and type
 
         :Example:
-        >>> _args_to_dict({"k1_k2_k3": v})
+        >>> _args_to_dict({"k1-k2-k3": v})
         {k1: {k2: {k3 : v}}}
 
     """
@@ -73,7 +79,7 @@ def _args_to_dict(d_arg, d_orig={}):
 
 class ListAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, yaml.safe_load("["+values+"]"))
+        setattr(namespace, self.dest, yaml.safe_load("[" + values + "]"))
 
 
 def pyonf(default_conf={}, mandatory_opts=[], argv=sys.argv[1:]):
@@ -100,43 +106,49 @@ def pyonf(default_conf={}, mandatory_opts=[], argv=sys.argv[1:]):
     file_conf = {}
     cli_conf = {}
 
-    log.debug("default_conf: %s (%s)" % (default_conf, type(default_conf)))
+    log.debug("default_conf: %s (%s)", default_conf, type(default_conf))
     if isinstance(default_conf, str):
         if os.path.isfile(default_conf):
             try:
                 log.debug(" open as YAML file")
                 conf = yaml.safe_load(open(default_conf))
-            except Exception as e:
-                print("pyonf: Cannot parse 'default_conf' argument:\n" +
-                      "%s is not a valid YAML file\n%s"
-                      % (default_conf, e),
-                      file=sys.stderr)
+            except Exception as ex:
+                print(
+                    "pyonf: Cannot parse 'default_conf' argument:\n"
+                    + "%s is not a valid YAML file\n%s" % (default_conf, ex),
+                    file=sys.stderr,
+                )
                 sys.exit(1)
         else:
             try:
                 conf = yaml.safe_load(default_conf)
-            except Exception as e:
-                log.debug(" open as YAML string" % default_conf)
-                print("pyonf: Cannot parse 'default_conf' argument:\n" +
-                      "%s is not a valid YAML string\n%s"
-                      % (default_conf, e),
-                      file=sys.stderr)
+            except Exception as ex:
+                log.debug(" open as YAML string")
+                print(
+                    "pyonf: Cannot parse 'default_conf' argument:\n"
+                    + "%s is not a valid YAML string\n%s" % (default_conf, ex),
+                    file=sys.stderr,
+                )
                 sys.exit(1)
         if not isinstance(conf, dict):
-            print("pyonf: Malformed 'default_conf' argument:\n" +
-                  "Parsed content is not a dict: %s" % conf,
-                  file=sys.stderr)
+            print(
+                "pyonf: Malformed 'default_conf' argument:\n"
+                + "Parsed content is not a dict: %s" % conf,
+                file=sys.stderr,
+            )
             sys.exit(1)
 
     elif isinstance(default_conf, dict):
-        log.debug(" is a dict" % default_conf)
+        log.debug("default conf is a dict")
         conf = default_conf.copy()
 
     else:
-        print("pyonf: Cannot parse 'default_conf' argument: \n" +
-              "%s is not a valid YAML file, string or Python dict object"
-              % default_conf,
-              file=sys.stderr)
+        print(
+            "pyonf: Cannot parse 'default_conf' argument: \n"
+            + "%s is not a valid YAML file, string or Python dict object"
+            % default_conf,
+            file=sys.stderr,
+        )
         sys.exit(1)
     log.debug(" content: %s" % conf)
 
@@ -155,35 +167,44 @@ def pyonf(default_conf={}, mandatory_opts=[], argv=sys.argv[1:]):
 
         pargs = ["--"+arg]
         if not arg[0] in short_args:
-            pargs.append("-"+arg[0])
+            pargs.append("-" + arg[0])
             short_args.add(arg[0])
-        pkwargs = {'action': 'store_true' if isinstance(val, bool) and not val
-                   else 'store_false' if isinstance(val, bool) and val
-                   else ListAction if isinstance(val, list)
-                   else 'store'}
-        if not isinstance(val, bool):
-            pkwargs['type'] = (
-                type(val) if val is not None and not isinstance(val, list)
-                else str
-                )
-        helpmsg = '*mandatory*, ' if arg in mandatory_opts else ""
-        if isinstance(val, (tuple, list)):
-            helpmsg += 'set elements of "%s" list, separated by ","' % \
-                       arg
-            pkwargs['metavar'] = '%s,%s,...' % \
-                                 (arg.upper()[0], arg.upper()[0])
-        elif isinstance(val, bool):
-            helpmsg += 'turn %s "%s"' % ('on' if not val else 'off', arg)
-        else:
-            helpmsg += 'set "%s" value, as %s' % \
-                       (arg, pkwargs['type'].__name__)
-        if val:
-            helpmsg += ' (%s %s)' % \
-                       ('e.g.,' if arg in mandatory_opts
-                        else 'default is ', val)
-        pkwargs['help'] = helpmsg
 
-        log.debug(" argparse parameters: %s, %s" % (pargs, pkwargs))
+        pkwargs = {}
+
+        if isinstance(val, bool) and not val:
+            pkwargs["action"] = "store_true"
+        elif isinstance(val, bool) and val:
+            pkwargs["action"] = "store_false"
+        elif isinstance(val, list):
+            pkwargs["action"] = ListAction
+        else:
+            pkwargs["action"] = "store"
+
+        if not isinstance(val, bool):
+            if val is not None and not isinstance(val, list):
+                pkwargs["type"] = type(val)
+            else:
+                pkwargs["type"] = str
+
+        if isinstance(val, (tuple, list)):
+            pkwargs["metavar"] = "%s,%s,..." % (arg.upper()[0], arg.upper()[0])
+
+        helpmsg = "*mandatory*, " if arg in mandatory_opts else ""
+        if isinstance(val, (tuple, list)):
+            helpmsg += 'set elements of "%s" list, separated by ","' % arg
+        elif isinstance(val, bool):
+            helpmsg += 'turn %s "%s"' % ("on" if not val else "off", arg)
+        else:
+            helpmsg += 'set "%s" value, as %s' % (arg, pkwargs["type"].__name__)
+        if val:
+            helpmsg += " (%s %s)" % (
+                "e.g.," if arg in mandatory_opts else "default is",
+                val,
+            )
+        pkwargs["help"] = helpmsg
+
+        log.debug(" argparse parameters: %s, %s", pargs, pkwargs)
         parser.add_argument(*pargs, **pkwargs)
 
     log.debug("parsing command line")
@@ -194,7 +215,7 @@ def pyonf(default_conf={}, mandatory_opts=[], argv=sys.argv[1:]):
                 if arg != 'conf_file' and val is not None}
     cli_conf = _args_to_dict(cli_conf, conf)
 
-    log.debug(" config from command line is: %s" % cli_conf)
+    log.debug(" config from command line is: %s", cli_conf)
 
     if cli_args.conf_file:
         file_conf = yaml.safe_load(cli_args.conf_file)
@@ -202,13 +223,13 @@ def pyonf(default_conf={}, mandatory_opts=[], argv=sys.argv[1:]):
 
     conf = _deep_update(conf, file_conf)
     conf = _deep_update(conf, cli_conf)
-    log.debug("full config is: %s" % conf)
+    log.debug("full config is: %s", conf)
 
-    for m in mandatory_opts:
-        if m not in [x for x, y in _dict_to_args(file_conf)] \
-           and m not in [x for x, y in _dict_to_args(cli_conf)]:
-            print('Error: "%s" option is not set' % m,
-                  file=sys.stderr)
+    for mopt in mandatory_opts:
+        if mopt not in [x for x, y in _dict_to_args(file_conf)] and mopt not in [
+            x for x, y in _dict_to_args(cli_conf)
+        ]:
+            print('Error: "%s" option is not set' % mopt, file=sys.stderr)
             sys.exit(1)
 
     return conf
